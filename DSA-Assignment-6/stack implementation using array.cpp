16dc4eaea37bfb1d696a670c7ed4229b5c634335
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2022-11-20 16:36:47 +0530

    Update stack implementation using array.cpp

--- DSA-Assignment-6/stack implementation using array.cpp
+++ DSA-Assignment-6/stack implementation using array.cpp
@@ -1,36 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct stack{
-	int A[1000];
-	int top=-1;
-} ;
-void Push(stack &s,int data){
-	s.top-- ;
-	s.A[s.top]=data;
-}
-void pop(stack &s){
-	s.top-- ;
-}
-int TOP(stack s){
-	return s.A[s.top];
-}
-int size(stack s){
-	return s.top+1;
-}
-bool isEMpty(stack s){
-	return s.top==-1;
-}
-int main(){
-	stack s;
-	Push(s,5);
-	pop(s);
-	Push(s,12);
+
+//complete the functions
+class Stack{
+	int A[100000];
+	int end;
 	
-	cout << "Top1:" << TOP(s) << endl ;
-	pop(s);
-	cout << "Top2:" << TOP(s) << endl ;
-	Push(s,17);
-	cout << "Top3:" << TOP(s) << endl ;
-	cout << "Size " << size(s) << endl;
+	public:
+	//constructor is called whenever an object of the class is declared
+	Stack(){
+		cout<<"constructor is Called\n";
+		end=-1;
+	}
+	
+	//push the value at the back
+	void push(int val){
+		cout<<"pushing the val: "<<val<<" at the back of queue\n";
+		if(end>=100000-1){
+			cout<< "stack overflow" << endl ;
+		}
+		else {
+			end++;
+			A[end]=val;
+		}
+	}
+	
+	int top(){
+		cout<<"returning the value at top or last\n";
+		return A[end];
+	}
+	
+	void pop(){
+		cout<<"removing the last element\n";
+		if(end<=-1){
+			cout<< "stack underflow" << endl;
+		}
+		else {
+			end--;
+		}
+	}
+	
+	int size(){
+		cout<<"return the size of the queue\n";
+		return end+1 ;
+	}
+	
+	bool isEmpty(){
+		cout<<"returning if the stack is empty\n";
+		return (end<0) ;
+	}
+};
+
+int main() {
+	Stack s; //constructor is called on this line
+	s.push(4);
+	cout<<"top: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
+	
+	s.push(5);
+	s.push(3);
+	cout<<"front: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
+	
+	cout<<"top: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
 	return 0;
 }
