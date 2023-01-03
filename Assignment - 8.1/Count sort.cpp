faf9ff7bc8265fec857c96da123f30558f96e112
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2023-01-03 21:25:31 +0530

    Create Count sort.cpp

---
+++ Assignment - 8.1/Count sort.cpp
@@ -0,0 +1,44 @@
+/*
+	Count Sort Algorithm
+	**Time Complexity: O(n)
+	**Space Complexity: O(max_number-min_number)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//a function that will print the vector passed to it, elements seprated by space
+void printVector(vector<int>A)
+{
+	//traverse all the element and print th
+	for(int i=0;i<A.size();i++)
+	{
+		cout<<A[i]<<" ";	
+	}
+	cout<<"\n";
+}
+
+//the vector A will be sorted inplace and that's why passed with refrence
+void sort(vector<int>&A)
+{ 	vector<int>B(100);
+	for(int i=0;i<A.size();i++){
+		B[A[i]]++;
+	}
+	int k=0;
+	for(int i=0;i<B.size();i++){
+		for(int j=0;j<B[i];j++){
+			A[k]=i;
+			k++;
+		}
+	}
+}
+
+int main() {
+	vector<int>A= {6,1,2,3,4,6,1,5,3,4,8};
+	// your code goes here
+	sort(A);
+	cout<<"sorted vector: ";
+	printVector(A);
+	
+	return 0;
+}
