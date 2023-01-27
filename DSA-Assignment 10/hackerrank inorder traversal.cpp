a6ce015b2434eaaee5fe0f4e7ad69a029ec51230
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2023-01-27 22:49:04 +0530

    Create hackerrank inorder traversal.cpp

---
+++ DSA-Assignment 10/hackerrank inorder traversal.cpp
@@ -0,0 +1,8 @@
+  void inOrder(Node *root) {
+        if(root==NULL){
+            return ;
+        }
+        inOrder(root->left);
+        cout<< root->data << " ";
+        inOrder(root->right); 
+    }
